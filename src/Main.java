b684fb62889cb96af94526b4bf47338f1422af12
tianlye <tianlye@hotmail.com>
2017-11-03 14:50:16 +0800

    Updated Main

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,49 @@
+import java.util.Scanner;
 
 public class Main {
+	String itemArray [];
 	//assume the list is what we use
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String input = "";
+		Main main = new Main();
+		while(!input.equalsIgnoreCase("1")){
+			main.displayMainMenu();
+			Scanner scanner = new Scanner(System. in); 
+			input = scanner. nextLine();
+			if(input.equals("0")){
+				System.out.println("Enter list of items: ");;
+				input = scanner. nextLine();
+				main.addItemViaString(input);
+				System.out.println("------------------------------------");;
+				
+			}
+		}
+	}
+	
+	public void displayMainMenu(){
+		System.out.println("------------------------------------");;
+		System.out.println("0 - enter order");;
+		System.out.println("1 - exit system");;
+		System.out.println("Input choice:");;
+	}
+	
+	
+	
+	public void displayAllItem(){
+		ProductCatalogue  catalogue = new ProductCatalogue();
+		System.out.println("Receipt:=======================");
+		for(int i = 0 ; i < itemArray.length; i++ )
+		{
+			
+			System.out.println(itemArray[i]+":"+catalogue);
+		}
+		System.out.println("=======================");
 		
 	}
 	
 	public String[] addItemViaString(String itemList){
-		String itemArray [] = itemList.split(" ");
+		itemArray  = itemList.split(" ");
 		for(int i = 0 ; i < itemArray.length; i++ )
 		{
 			System.out.println(itemArray[i]);
@@ -17,3 +53,4 @@ public class Main {
 	}
 
 }
+
