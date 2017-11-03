0292b373f8e12462c9d9142326b5a397714c154e
issuser <issuser@ISSCR2929.stf.nus.edu.sg>
2017-11-03 15:13:53 +0800

    Multi item discount engine for two items

--- src/MultiItemDiscountEngine.java
+++ src/MultiItemDiscountEngine.java
@@ -6,12 +6,27 @@ public class MultiItemDiscountEngine extends DiscountEngine {
 	}
 	
 	public DiscountedItem[] calculateDiscounts(String[] items) {
-		if(items.length == 1) {
-			DiscountedItem[] result = new DiscountedItem[1];
-			result[0] = new DiscountedItem(items[0], 0, 0);
-			return result;
+		int numOfItems = items.length;
+		DiscountedItem[] result = new DiscountedItem[numOfItems];
+		for(int i = 0; i < numOfItems; i++) {
+			result[i] = new DiscountedItem(items[i], 0, 0);
 		}
-		DiscountedItem[] result = new DiscountedItem[0];
+		
+		
+//		if(numOfItems == 1) {
+//			result[0].setD
+//			result[0] = new DiscountedItem(items[0], 0, 0);
+//			return result;
+//		}
+		
+		
+		
+		if(numOfItems == 2) {
+			if(items[0].equals(items[1])) {
+				result[1].setDiscount(30);
+			}
+		}
+		
 		return result;
 	}
 
--- test/MultiItemDiscountEngineTest.java
+++ test/MultiItemDiscountEngineTest.java
@@ -15,5 +15,31 @@ public class MultiItemDiscountEngineTest extends TestCase {
 		DiscountedItem firstItem = discountedItems[0];
 		assertTrue(firstItem.getDiscount() == 0);
 	}
+	
+	@Test
+	public void testForTwoSimilarItems() {
+		String[] items = new String[2];
+		items[0] = "hello";
+		items[1] = "hello";
+		DiscountEngine d = new MultiItemDiscountEngine();
+		DiscountedItem[] discountedItems = d.calculateDiscounts(items);
+		DiscountedItem firstItem = discountedItems[0];
+		DiscountedItem secondItem = discountedItems[1];
+		assertTrue(firstItem.getDiscount() == 0);
+		assertTrue(secondItem.getDiscount() == 30);
+	}
+	
+	@Test
+	public void testForTwoDifferentItems() {
+		String[] items = new String[2];
+		items[0] = "hello";
+		items[1] = "hallo";
+		DiscountEngine d = new MultiItemDiscountEngine();
+		DiscountedItem[] discountedItems = d.calculateDiscounts(items);
+		DiscountedItem firstItem = discountedItems[0];
+		DiscountedItem secondItem = discountedItems[1];
+		assertTrue(firstItem.getDiscount() == 0);
+		assertTrue(secondItem.getDiscount() == 0);
+	}
 
 }
