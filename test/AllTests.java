071db667f4d70a33f9b12302141b18775484a19b
issuser <issuser@ISSCR2929.stf.nus.edu.sg>
2017-11-03 14:00:28 +0800

    initial build setup for Jenkins

---
+++ test/AllTests.java
@@ -0,0 +1,15 @@
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+
+public class AllTests extends TestCase {
+	public static TestSuite suite() {
+		TestSuite suite = new TestSuite("Test for default package");
+		//$JUnit-BEGIN$
+		suite.addTestSuite(DummyTest.class);
+//		suite.addTestSuite(SalesEmployeeTest.class);
+		//$JUnit-END$
+		return suite;
+	}
+}
